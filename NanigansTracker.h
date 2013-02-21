436ccc2803f0f06a46a7402f7b74128c8655f0f1
Darko Pavlovic <darkop@nordeus.eu>
2013-02-21 18:21:52 +0100

    NanigansTracker class created.

---
+++ NanigansTracker.h
@@ -0,0 +1,13 @@
+//
+//  NanigansTracker.h
+//  StariKocijas
+//
+//  Created by Darko Pavlovic on 2/21/13.
+//  Copyright (c) 2013 Nordeus. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NanigansTracker : NSObject
+
+@end
